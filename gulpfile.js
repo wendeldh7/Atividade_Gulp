48ae5fc8d586ddf6018c605d8142f8579b3d2c49
Daniela Mical <daniela.m.torrez14@gmail.com>
2023-03-27 18:25:23 -0400

    Primeiro commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
 
 function funcaoPadrao(callback) {
     setTimeout(function() {
@@ -21,3 +33,7 @@ function dizTchau() {
 
 exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+}
